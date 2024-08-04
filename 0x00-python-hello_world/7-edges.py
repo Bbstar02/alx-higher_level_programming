2b5a2274e64f9ae68c45ea2eb621caa8963a2a84
Blessbae01 <uzoukwublessing34@gmail.com>
2024-08-04 03:09:26 +0100

    bb

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
-# Extracting parts of the word for a sweet output
-word_first_3 = word[:3]
-word_last_2 = word[-2:]
-middle_word = word[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+word = "Holberton"  # Define the word variable
+word_first_3 = word[:3]  # Extract the first 3 letters
+word_last_2 = word[-2:]  # Extract the last 2 letters
+middle_word = word[1:-1]  # Extract the middle part of the word
+print(f"First 3 letters: {word_first_3}")  # Print the first 3 letters
+print(f"Last 2 letters: {word_last_2}")  # Print the last 2 letters
+print(f"Middle word: {middle_word}")  # Print the middle part of the word
