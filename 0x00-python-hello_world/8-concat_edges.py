10cce76a20a5721391d5b06b18659c42c18ff6f1
Blessbae01 <uzoukwublessing34@gmail.com>
2024-08-04 03:18:56 +0100

    bb

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-# Define the string variable with a long sentence
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Concatenate specific slices of the string and assign to str
-str = str[39:66] + str[106:112] + str[:6]
-# Print the concatenated result
-print(str)
+print(str[39:66] + str[106:112] + str[:6])
