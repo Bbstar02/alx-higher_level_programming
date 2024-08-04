e2c184e5e8547a02aee412c254aba5674c86ca31
Blessbae01 <uzoukwublessing34@gmail.com>
2024-08-04 05:02:43 +0100

    bup

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_calculation(a, b):
-    # This function adds 98 to the result of b raised to the power of a
-    return 98 + (b ** a)
+#my code is going to give the correct result
+    return 98 + (a ** b)
