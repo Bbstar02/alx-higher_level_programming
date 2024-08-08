9a863ec330a560ad50e1548c973f5cb60e9ce118
Blessbae01 <uzoukwublessing34@gmail.com>
2024-08-08 14:19:17 +0100

    hhhj

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Loop through numbers from 0 to 98
+for decimal in range(0, 99):
+    # Print the number and its hexadecimal representation
+    print('{0} = {1}'.format(decimal, hex(decimal)))
+
