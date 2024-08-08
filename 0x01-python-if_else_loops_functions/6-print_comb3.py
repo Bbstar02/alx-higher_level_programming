f4cf72a19000eda897f4795acdaab153a909b877
Blessbae01 <uzoukwublessing34@gmail.com>
2024-08-08 14:54:11 +0100

    mine

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
-print("{:d}{:d}".format(i + 1, j))
+for first_digit in range(0, 8):
+    for second_digit in range(first_digit + 1, 10):
+        print("{:d}{:d}".format(first_digit, second_digit), end=', ')
+print("{:d}{:d}".format(first_digit + 1, second_digit))
+
